18af1ba6a175f4a35b87f81cc142921e1c525f44
HB <85717438+HBAMAL@users.noreply.github.com>
2021-08-17 14:30:23 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -13,57 +13,49 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 
 
-START_TEXT = """HI I AM  TELSA URL UPLOADER BOT,
+START_TEXT = """HI {}I AM  TELSA URL UPLOADER BOT,
+
 MADE BY @TELSABOTS
 """
-HELP_USER = """It's not that complicated to use meh! 😅
-    
-1. Send Me A Tumbnail if required. It'll be saved permanently.💯
-2. If Thumbnail Wasn't Set By You, It'll Be Auto Generated From The File.🥳
-3. Send Me Any Link To Be Uploaded To Telegram.
-4. Press /deletthumbnail To Delete Your Current Custom Thumbnail
-5. Select the button.
-   SVideo - Give File as video with Screenshots
-   DFile  - Give File with Screenshots
-   Video  - Give File as video without Screenshots
-   DFile  - Give File without Screenshots
-NB : It is Recommended To Use A Custom Thumbnail Because, Some Time Won't Upload The File Without a Custom Thumbnail.
-Support Group : @TeleRoid14
+
+HELP_TEXT = """
+</b>SENT ANY LINK.......</b>
+
+THAT'S ALL........
+
+</b>MADE BY </b>@TELSABOTS
 """
 
-ABOUT_TEXT = """<b>🤖 BOT🤖 : TELSA URL UPLOADER BOT</b>
-<b>🧑🏼‍💻DEV 🧑🏼‍💻 : 👉 <a href='https://t.me/ALLUADDICT'> ꧁༒☬𝓗𝓑☬༒꧂ </a></b>
-<b> 📢CHANNEL📢 : 👉 <a href='https://t.me/TELSABOTS'> TELSA BOTS </a></b>"""
+ABOUT_TEXT = """
+🤖</b>BOT🤖 : TELSA MEDIA INFO BOT</b>
+ 
+📢</b>CHANNEL📢 :</b> ❤️ <a href='https://t.me/telsabots'>TELSA BOTS</a>
+🧑🏼‍💻DEV🧑🏼‍💻: @ALLUADDICT
+__🤩SOURCE🤩:__ [👉CLICK HERE👈](https://t.me/SOURCE_TELSA/7)
+"""
 START_BUTTONS = InlineKeyboardMarkup(
         [[
         InlineKeyboardButton('📢CHANNEL📢', url='https://telegram.me/TELSABOTS'),
         InlineKeyboardButton('🧑🏼‍💻DEV🧑🏼‍💻', url='https://telegram.me/alluaddict')
         ],[
-        InlineKeyboardButton('🆘HELP🆘', callback_data='help'),
-        InlineKeyboardButton('🤗ABOUT🤗', callback_data='about'),
-        InlineKeyboardButton('🔐CLOSE🔐', callback_data='close')
+        InlineKeyboardButton('🤩YOUTUBE🤩', url='http://www.youtube.com/watch?v=nfWjbuQqgJc')
         ]]
     )
 
 HELP_BUTTONS = InlineKeyboardMarkup(
-        [[
+       [[
         InlineKeyboardButton('📢CHANNEL📢', url='https://telegram.me/TELSABOTS'),
         InlineKeyboardButton('🧑🏼‍💻DEV🧑🏼‍💻', url='https://telegram.me/alluaddict')
         ],[
-        InlineKeyboardButton('🏡HOME🏡', callback_data='home'),
-        InlineKeyboardButton('🤗ABOUT🤗', callback_data='about'),
-        InlineKeyboardButton('🔐CLOSE🔐', callback_data='close')
+        InlineKeyboardButton('🤩YOUTUBE🤩', url='http://www.youtube.com/watch?v=nfWjbuQqgJc')
         ]]
     )
-  
 ABOUT_BUTTONS = InlineKeyboardMarkup(
         [[
         InlineKeyboardButton('📢CHANNEL📢', url='https://telegram.me/TELSABOTS'),
         InlineKeyboardButton('🧑🏼‍💻DEV🧑🏼‍💻', url='https://telegram.me/alluaddict')
         ],[
-        InlineKeyboardButton('🏡HOME🏡', callback_data='home'),
-        InlineKeyboardButton('🆘HELP🆘', callback_data='help'), 
-        InlineKeyboardButton('🔐CLOSE🔐', callback_data='close')
+        InlineKeyboardButton('🤩YOUTUBE🤩', url='http://www.youtube.com/watch?v=nfWjbuQqgJc')
         ]]
     )
 @Client.on_callback_query()
@@ -104,7 +96,7 @@ async def help_message(bot, update):
     
 @Client.on_message(filters.command(["start"]))
 async def start(bot, update):
-    text = START_TEXT,
+    text = START_TEXT.format(update.from_user.mention),
     reply_markup = START_BUTTONS
     await update.reply_text(
         text=text,
